e83446ea27ff929c92b019be91f28406e0ea8a69
Mostafa Aswany <33361106+MostafaAswany@users.noreply.github.com>
2018-01-27 21:06:57 +0200

    friendship day card
    happy friendship day card android app

---
+++ app/src/main/java/happyfriendshipday/mostafa/example/com/happyfriendshipday/MainActivity.java
@@ -0,0 +1,32 @@
+package happyfriendshipday.mostafa.example.com.happyfriendshipday;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button bt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bt = (Button) findViewById(R.id.button);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(Intent.ACTION_SEND);
+                myIntent.setType("text/plain");
+                String shareBody = "Your body here";
+                String shareSub = "Your subject here";
+                myIntent.putExtra(Intent.EXTRA_SUBJECT, shareSub);
+                myIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(myIntent, "Share It With"));
+
+            }
+        });
+    }
+}
